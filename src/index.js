6cf4cd8bae51d779dd7ed1a71601a16a5fae913b
alexsilvafranca <alex.silvafranca@hotmail.com>
2024-11-22 23:35:27 -0300

    Entrega do desafio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-let nome = "felipe";
-console.log("oi");
+let meuHeroi = "Felipão";
+let XP = 4500;
+
+function contador() {
+    let nivel = "";
+
+    if (XP <= 1000) {
+        nivel = "Ferro";
+    } else if (XP >= 1001 && XP <= 2000) {
+        nivel = "Bronze";
+    } else if (XP >= 2001 && XP <= 5000) {
+        nivel = "Prata";
+    } else if (XP >= 5001 && XP <= 7000) {
+        nivel = "Ouro";
+    } else if (XP >= 7001 && XP <= 8000) {
+        nivel = "Platina";
+    } else if (XP >= 8001 && XP <= 9000) {
+        nivel = "Ascendente";
+    } else if (XP >= 9001 && XP <= 10000) {
+        nivel = "Imortal";
+    } else if (XP > 10000) {
+        nivel = "Radiante";
+    }
+
+    // Retorna a mensagem final com o nível
+    console.log(`O Herói de nome ${meuHeroi} está no nível de ${nivel}`);
+}
+
+contador();
